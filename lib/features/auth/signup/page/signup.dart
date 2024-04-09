7fe4975768862134c41ddf1e899b9d827edf280d
Anas Hany <anashany219@gmail.com>
2024-04-09 21:03:20 +0200

    Created SignUp Page ✨

--- lib/features/auth/signup/page/signup.dart
+++ lib/features/auth/signup/page/signup.dart
@@ -1,3 +1,7 @@
+import 'package:expense_tracker/core/text_style.dart';
+import 'package:expense_tracker/features/auth/login/component/primary_button.dart';
+import 'package:expense_tracker/features/auth/login/page/login.dart';
+import 'package:expense_tracker/features/auth/signup/component/signup_form.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -5,6 +9,71 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 70,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text('Create New Account', style: titleText),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: SignUpForm(),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: PrimaryButton(
+                buttonText: 'Confirm',
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [
+                  Text(
+                    'have an account ? ',
+                    style: subTitle,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScrean(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Login',
+                      style: textButton.copyWith(
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
