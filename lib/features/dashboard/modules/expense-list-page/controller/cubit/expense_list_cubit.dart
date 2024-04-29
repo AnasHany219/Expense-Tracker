e40ca0724f56eed8da4f4a28b5d29b90023b427a
Islam Sobhy <islam1sobhy123@gmail.com>
2024-04-29 08:01:37 +0300

    ignore_for_file

--- lib/features/dashboard/modules/expense-list-page/controller/cubit/expense_list_cubit.dart
+++ lib/features/dashboard/modules/expense-list-page/controller/cubit/expense_list_cubit.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:bloc/bloc.dart';
 import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/expense.dart';
 import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/repo/local_db_data.dart';
