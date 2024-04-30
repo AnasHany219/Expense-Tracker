bc351cd55d335b723e331ce855f7464f9a768d54
Islam Sobhy <islam1sobhy123@gmail.com>
2024-05-01 02:24:05 +0300

    Refactor ExpenseListPage components 💡

--- lib/features/dashboard/modules/expense-list-page/view/components/expense_list_item.dart
+++ lib/features/dashboard/modules/expense-list-page/view/components/expense_list_item.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/expense.dart';
 
+/// Widget representing a single expense item in the list.
 class ExpenseListItem extends StatelessWidget {
+  /// The expense item to display.
   final Expense expense;
+
+  /// Callback function triggered when the remove button is pressed.
   final VoidCallback onRemove;
 
+  /// Constructs a new [ExpenseListItem] instance.
   const ExpenseListItem(
       {super.key, required this.expense, required this.onRemove});
 
