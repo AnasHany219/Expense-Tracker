4fee94389307f46b2b46be8e72c7812562696fe7
Islam Sobhy <islam1sobhy123@gmail.com>
2024-05-01 02:29:28 +0300

    Refactor ExpenseListCubit for better readability and documentation 🛠️

--- lib/features/dashboard/modules/expense-list-page/controller/cubit/expense_list_cubit.dart
+++ lib/features/dashboard/modules/expense-list-page/controller/cubit/expense_list_cubit.dart
@@ -2,20 +2,25 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/expense.dart';
-// import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/repo/firebase_data.dart';
-import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/repo/local_db_data.dart';
+import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/repo/local_db_data.dart'; // import 'package:expense_tracker/features/dashboard/modules/expense-list-page/model/repo/firebase_data.dart';
 import 'package:flutter/material.dart';
 
 part 'expense_list_state.dart';
 
+/// Cubit for managing the state of the expense list.
 class ExpenseListCubit extends Cubit<ExpenseListState> {
+  /// The email address of the user.
   final String? email;
-  List<Expense> expenses = []; // List variable to store expenses
 
+  /// List variable to store expenses.
+  List<Expense> expenses = [];
+
+  /// Constructs a new [ExpenseListCubit] instance.
   ExpenseListCubit(this.email) : super(ExpenseListLoading()) {
     init();
   }
 
+  /// Initializes the cubit.
   Future<void> init() async {
     emit(ExpenseListLoading());
     expenses = await (await DatabaseRepo.instance).fetchExpenses(email: email);
@@ -24,10 +29,12 @@ class ExpenseListCubit extends Cubit<ExpenseListState> {
     emit(expenses.isEmpty ? ExpenseListEmpty() : ExpenseListLoaded());
   }
 
+  /// Removes an expense.
   Future<void> removeExpense(BuildContext context, String id) async {
     try {
-      // Call the deleteExpense method from local database
       final DatabaseRepo dbInstance = await DatabaseRepo.instance;
+
+      // Call the deleteExpense method from local database
       await dbInstance.deleteExpense(id: id);
 
       // Call the deleteExpense method from firebase
