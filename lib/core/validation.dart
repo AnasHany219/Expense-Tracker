b6b1a5a784cd1a67b6d14c611f98bfc6930e8d47
Anas Hany <anashany219@gmail.com>
2024-05-04 00:38:21 +0300

    Refactor Validator class for improved readability and organization 🛠️

--- lib/core/validation.dart
+++ lib/core/validation.dart
@@ -1,28 +1,25 @@
 class Validator {
+  /// Validates the provided name.
   String? nameValidator(String? value) {
-    // Regular expression for firstname and lastname validation
-    final nameRegex = RegExp(r'^[a-zA-Z]+$');
-
     if (value == null || value.isEmpty) {
       return 'Please enter your name';
-    } else if (!nameRegex.hasMatch(value)) {
+    } else if (!_isValidName(value)) {
       return 'Please enter a valid name';
     }
     return null;
   }
 
+  /// Validates the provided email address.
   String? emailValidator(String? value) {
-    // Regular expression for email validation
-    final emailRegex = RegExp(r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
-
     if (value == null || value.isEmpty) {
       return 'Please enter an email address';
-    } else if (!emailRegex.hasMatch(value)) {
+    } else if (!_isValidEmail(value)) {
       return 'Please enter a valid email address';
     }
     return null;
   }
 
+  /// Validates the provided password.
   String? passwordValidator(String? value) {
     if (value == null || value.isEmpty) {
       return 'Please enter a password';
@@ -32,6 +29,7 @@ class Validator {
     return null;
   }
 
+  /// Validates the confirmation password.
   String? confirmPasswordValidator(String? value, String password) {
     if (value != password) {
       return 'Passwords do not match';
@@ -39,19 +37,33 @@ class Validator {
     return null;
   }
 
+  /// Validates the provided OTP.
   String? otpValidator(String? value) {
     if (value == null || value.isEmpty) {
       return 'Please enter the OTP';
-    } else if (value.length != 4 || !isNumeric(value)) {
+    } else if (value.length != 4 || !_isNumeric(value)) {
       return 'OTP must be a 4-digit number';
     }
     return null;
   }
 
-  bool isNumeric(String? s) {
+  /// Checks if a string is numeric.
+  bool _isNumeric(String? s) {
     if (s == null) {
       return false;
     }
     return double.tryParse(s) != null;
+  }
+
+  /// Checks if the provided name is valid.
+  bool _isValidName(String value) {
+    final nameRegex = RegExp(r'^[a-zA-Z]+$');
+    return nameRegex.hasMatch(value);
+  }
+
+  /// Checks if the provided email is valid.
+  bool _isValidEmail(String value) {
+    final emailRegex = RegExp(r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    return emailRegex.hasMatch(value);
   }
 }
