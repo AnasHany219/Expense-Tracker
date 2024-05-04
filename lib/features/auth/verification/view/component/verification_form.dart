99e5f47195bdfdf7700137d79049187103391205
Anas Hany <anashany219@gmail.com>
2024-05-04 18:53:57 +0300

    ✨ Refactor VerificationForm widget for readability and reusability

--- lib/features/auth/verification/view/component/verification_form.dart
+++ lib/features/auth/verification/view/component/verification_form.dart
@@ -1,9 +1,9 @@
-import 'package:expense_tracker/core/validation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:expense_tracker/core/text_style.dart';
-import 'package:expense_tracker/features/auth/verification/controller/cubit/verification_cubit.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:expense_tracker/features/auth/verification/controller/cubit/verification_cubit.dart';
+import 'package:expense_tracker/core/validation.dart';
+import 'package:expense_tracker/core/text_style.dart';
 
 class VerificationForm extends StatefulWidget {
   final String? email;
@@ -16,78 +16,96 @@ class VerificationForm extends StatefulWidget {
 }
 
 class _VerificationFormState extends State<VerificationForm> {
-  VerificationCubit controller = VerificationCubit();
+  late VerificationCubit _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VerificationCubit();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider.value(
-      value: controller,
+      value: _controller,
       child: BlocBuilder<VerificationCubit, VerificationState>(
         builder: (context, state) {
-          return Form(
-            key: controller.formKey,
-            child: Column(
-              children: [
-                PinCodeTextField(
-                  controller: controller.verificationControllers,
-                  appContext: context,
-                  length: 4,
-                  obscureText: false,
-                  animationType: AnimationType.fade,
-                  keyboardType: TextInputType.number,
-                  validator: Validator().otpValidator,
-                  onChanged: (value) {},
-                  onCompleted: (value) {
-                    controller.verifyValidate(context, widget.email!,
-                        password: widget.password);
-                  },
-                  pinTheme: PinTheme(
-                    shape: PinCodeFieldShape.box,
-                    borderRadius: BorderRadius.circular(5),
-                    fieldHeight: 50,
-                    fieldWidth: 40,
-                    inactiveFillColor: primaryColor,
-                    inactiveColor: primaryColor,
-                    activeFillColor: Colors.white,
-                  ),
-                  cursorColor: Colors.black,
-                  animationDuration: const Duration(milliseconds: 300),
-                  backgroundColor: Colors.transparent,
-                  enableActiveFill: true,
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t receive any code ? ',
-                      style: subTitle,
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        controller.clear();
-                        controller.resendOTP(context, widget.email!);
-                      },
-                      child: Text(
-                        'Resend',
-                        style: textButton.copyWith(
-                          decoration: TextDecoration.underline,
-                          decorationThickness: 1,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
+          return buildForm();
         },
       ),
+    );
+  }
+
+  Widget buildForm() {
+    return Form(
+      key: _controller.formKey,
+      child: Column(
+        children: [
+          buildPinCodeTextField(),
+          const SizedBox(height: 5),
+          buildResendCodeText(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPinCodeTextField() {
+    return PinCodeTextField(
+      controller: _controller.verificationControllers,
+      appContext: context,
+      length: 4,
+      obscureText: false,
+      animationType: AnimationType.fade,
+      keyboardType: TextInputType.number,
+      validator: Validator().otpValidator,
+      onChanged: (value) {},
+      onCompleted: (value) {
+        _controller.verifyValidate(context, widget.email!,
+            password: widget.password);
+      },
+      pinTheme: buildPinTheme(),
+      cursorColor: Colors.black,
+      animationDuration: const Duration(milliseconds: 300),
+      backgroundColor: Colors.transparent,
+      enableActiveFill: true,
+    );
+  }
+
+  PinTheme buildPinTheme() {
+    return PinTheme(
+      shape: PinCodeFieldShape.box,
+      borderRadius: BorderRadius.circular(5),
+      fieldHeight: 50,
+      fieldWidth: 40,
+      inactiveFillColor: primaryColor,
+      inactiveColor: primaryColor,
+      activeFillColor: Colors.white,
+    );
+  }
+
+  Widget buildResendCodeText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Don\'t receive any code ? ',
+          style: subTitle,
+        ),
+        const SizedBox(width: 5),
+        GestureDetector(
+          onTap: () {
+            _controller.clear();
+            _controller.resendOTP(context, widget.email!);
+          },
+          child: Text(
+            'Resend',
+            style: textButton.copyWith(
+              decoration: TextDecoration.underline,
+              decorationThickness: 1,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
